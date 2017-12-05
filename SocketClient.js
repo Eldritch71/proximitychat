5b868a58adf76ac9ef09dea7dd1b767415e6296b
reldredge10 <reldredge10@gmail.com>
2017-12-05 17:43:49 -0500

    Copying Previous Work

---
+++ SocketClient.js
@@ -0,0 +1,62 @@
+﻿$(function () {
+    var socket = io();
+                
+    /*test geolocation*/
+    if(navigator.gelocation){
+        alert("browser supports geolocation");
+
+        /*now confirm that geolocation is enabled by user.  If not, throw error*/
+        navigator.geolocation.getCurrentPosition(function(p){
+            alert("geolocation enabled");
+
+            /*set a listener for location requests*/
+            socket.on('request location', function(){
+                alert("location requested");
+                navigator.getCurrentPosition(function(position){
+                    socket.emit('send location', {lat : position.coords.latitude, lng: position.coords.longitude, });
+                }, showError(error));
+            });
+        }, showError(error));
+            
+    /*send alert if geolocation not supported by browser*/
+    } else {
+        alert("Geolocation is not supported by this broswer");
+    }
+
+
+    /*fired when form button is pressed*/
+    $('form').submit(function(){
+        socket.emit('message to server', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+
+    /*fired when client receives message from server*/
+    socket.on('message to clients', function(msg){
+        $('#messages').append($('<li>').text(msg));
+    });
+
+    /*fired when message is declined by server*/
+    socket.on('decline message', function(msg){
+        alert(msg);
+    });
+});
+
+
+function showError(error) {
+    alert("there was an error");
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.");
+            break;
+    }
+}
---
+++ index.js
@@ -0,0 +1,49 @@
+﻿var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+var inRange = true;
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+/*sets up a listener for a particular event
+when the event is received, the function is called*/
+
+/*fired when client connects to server*/
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    /*fired when client disconnects from server*/
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    /*fired when server receives message from client*/
+    socket.on('message to server', function(msg){
+        console.log('message: ' + msg);
+        /*if inRange = true, then broadcast message to all clients*/
+        if(inRange){
+            socket.emit('request location');
+            io.emit('message to clients', msg);
+         }
+        /*else, send decline message to sender*/
+        else {
+            socket.emit('decline message', "sorry, but you are not close enough to recipient(s)")
+        }
+    });
+
+    socket.on('send location', function(data){
+        console.log(data.lat + ", " + data.lng)});
+});
+
+/*check the locations of each connected user in real time*/
+/*when changes from in range to out of range, send event "out of range" to all clients*/
+    /*also set boolean to false*/
+/*when changes from out of range to in range, send event "in range" to all clients*/
+    /*also set boolean to true*/
+
+http.listen(3000, function(){
+    console.log('listening on *3000');
+});
