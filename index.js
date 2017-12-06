b45224a10be4ba60dffe48882266c37dc14088f8
reldredge10 <reldredge10@gmail.com>
2017-12-06 00:13:23 -0500

    reformatting

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 ﻿var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var http = require('http').Server(app);
+var io = require('socket.io');
+var webSocket = io.listen(http);
 
 var inRange = true;
+var connectedUsers = {};
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -15,23 +17,47 @@ when the event is received, the function is called*/
 io.on('connection', function(socket){
     console.log('a user connected');
 
+    /*extra setup for client connecting*/
+    socket.on("join", function(user, sendKey){
+        user.key = Date.now();
+        connectedUsers[user.key] = user;
+        sendKey(user.key);
+        socket.set('userkey', user.key);
+        /*now broadcast the client connecting to all other clients*/
+        socket.broadcast.emit('user connected', user);
+    });
+
     /*fired when client disconnects from server*/
     socket.on('disconnect', function(){
-        console.log('user disconnected');
+        socket.get('userkey', function(err, key){
+            var user = connectedUsers[key];
+            /*is user in connectedUsers?*/
+            if(user){
+                delete connectedUsers[key];
+                socket.broadcast.emit("user disconnected", key);
+                console.log('user disconnected');
+            }
+        });
     });
 
     /*fired when server receives message from client*/
     socket.on('message to server', function(msg){
-        console.log('message: ' + msg);
-        /*if inRange = true, then broadcast message to all clients*/
-        if(inRange){
-            socket.emit('request location');
-            io.emit('message to clients', msg);
-         }
-        /*else, send decline message to sender*/
-        else {
-            socket.emit('decline message', "sorry, but you are not close enough to recipient(s)")
-        }
+        socket.get('userkey', function(err, key){
+            /*test 1: is user listed in connectedUsers?*/
+            var user = connectedUsers[key];
+            if(user){
+                /*test 2: is everyone within range of each other?*/
+                if(inRange){
+                    console.log('message: ' + msg);
+                    var data = { key: key, sender: user.name, message: msg };
+                    io.emit('message to clients', data);
+                } else {
+                    socket.emit('decline message', "sorry, but you are not close enough to recipient(s)");
+                }
+            } else {
+                socket.emit('decline message', "sorry, but you are not connected to the group");
+            }
+        });
     });
 
     socket.on('send location', function(data){
